98ab2f8d690e29bbad4ebb9cb3d32899fd5ce349
Troy Figiel <troy.figiel@gmail.com>
2022-06-15 09:12:53 +0200

    One step further with flakes

--- flake.nix
+++ flake.nix
@@ -1,5 +1,22 @@
 {
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    #emacs-config.url = "gitlab:troy.figiel/emacs";
   };
+
+
+  outputs = inputs@{ self, nixpkgs, home-manager }:
+    let
+      system = "x86_64-linux";
+      username = "troy";
+    in {
+      homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
+        configuration = import ./home.nix;
+        inherit system username;
+        homeDirectory = "/home/${username}";
+        stateVersion = "22.05";
+      };
+    };
 }
