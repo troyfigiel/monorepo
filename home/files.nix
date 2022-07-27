b362f18f1dc73f137c806019f3a40cfa466b2852
Troy Figiel <troy.figiel@gmail.com>
2022-07-27 19:37:05 +0200

    Clean up left-over config files

--- home/files.nix
+++ home/files.nix
@@ -2,9 +2,5 @@
   home.file = {
     ".config/user-dirs.dirs".source = ./config/user-dirs.dirs;
     ".config/user-dirs.locale".source = ./config/user-dirs.locale;
-    ".config/direnv".source = ./config/direnv;
-    ".config/pypoetry".source = ./config/pypoetry;
-    ".gnupg/gpg.conf".source = ./config/gnupg/gpg.conf;
-    ".gnupg/gpg-agent.conf".source = ./config/gnupg/gpg-agent.conf;
   };
 }
