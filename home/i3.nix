81fa3bc267de58399d5f511ef7603ee852936ced
Troy Figiel <troy.figiel@gmail.com>
2022-08-02 00:08:56 +0200

    Introduce variable

--- home/i3.nix
+++ home/i3.nix
@@ -15,7 +15,23 @@
         };
         terminal = "${pkgs.alacritty}/bin/alacritty";
         menu = "${pkgs.rofi}/bin/rofi -modi drun -show drun";
-        keybindings = lib.mkOptionDefault {
+        keybindings =
+        let
+          modifier = config.xsession.windowManager.i3.config.modifier;
+        in lib.mkOptionDefault {
+          "${modifier}+h" = "focus left";
+          "${modifier}+j" = "focus down";
+          "${modifier}+k" = "focus up";
+          "${modifier}+l" = "focus right";
+
+          "${modifier}+v" = "split vertical";
+          "${modifier}+z" = "split horizontal";
+
+          "${modifier}+Shift+h" = "move left";
+          "${modifier}+Shift+j" = "move down";
+          "${modifier}+Shift+k" = "move up";
+          "${modifier}+Shift+l" = "move right";
+
           "XF86AudioMute" = "exec ${pkgs.alsa-utils}/bin/amixer set Master toggle";
           "XF86AudioLowerVolume" = "exec ${pkgs.alsa-utils}/bin/amixer set Master 4%-";
           "XF86AudioRaiseVolume" = "exec ${pkgs.alsa-utils}/bin/amixer set Master 4%+";
