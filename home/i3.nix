911c92f2316c0e609313ea8fbd90655e3622e97a
Troy Figiel <troy.figiel@gmail.com>
2022-08-06 10:36:03 +0200

    Change some more keybindings

--- home/i3.nix
+++ home/i3.nix
@@ -15,66 +15,71 @@
         window.border = 2;
 
         gaps = {
-          inner = 10;
-          outer = 0;
+          inner = 8;
+          outer = -4;
           smartGaps = true;
           smartBorders = "on";
         };
 
         terminal = "${pkgs.alacritty}/bin/alacritty";
 
-        keybindings = { # I do not necessarily need the defaults, right? lib.mkOptionDefault {
+        # TODO: Do not depend on the defaults.
+        # I have too many keybindings here to know what is default and what is mine.
+        keybindings = lib.mkOptionDefault {
           "${modifier}+h" = "focus left";
           "${modifier}+j" = "focus down";
           "${modifier}+k" = "focus up";
           "${modifier}+l" = "focus right";
+
+          "${modifier}+n" = "workspace next";
+          "${modifier}+p" = "workspace prev";
 
           "${modifier}+v" = ''
             split vertical; exec ${pkgs.libnotify}/bin/notify-send "Tiling vertically."'';
           "${modifier}+z" = ''
             split horizontal; exec ${pkgs.libnotify}/bin/notify-send "Tiling horizontally."'';
 
-          "${modifier}+Shift+h" = "move left";
-          "${modifier}+Shift+j" = "move down";
-          "${modifier}+Shift+k" = "move up";
-          "${modifier}+Shift+l" = "move right";
+          "${modifier}+Ctrl+h" = "move left";
+          "${modifier}+Ctrl+j" = "move down";
+          "${modifier}+Ctrl+k" = "move up";
+          "${modifier}+Ctrl+l" = "move right";
 
           "${modifier}+m" = "exec ${pkgs.rofi}/bin/rofi -modi drun -show drun";
           # This is like a buffer switch in Emacs
           "${modifier}+b" = "exec ${pkgs.rofi}/bin/rofi -show window";
           # "${modifier}+Shift+g" = "mode gaps";
           "${modifier}+q" = "kill";
+
           "${modifier}+Shift+q" =
             "exec ${pkgs.betterlockscreen}/bin/betterlockscreen --lock blur";
-
-          "${modifier}+Ctrl+b" = "exec ${pkgs.firefox}/bin/firefox";
-          "${modifier}+Ctrl+d" =
+          "${modifier}+Shift+b" = "exec ${pkgs.firefox}/bin/firefox";
+          "${modifier}+Shift+d" =
             "exec ${terminal} -e ${pkgs.lazydocker}/bin/lazydocker";
-          "${modifier}+Ctrl+f" = "exec ${terminal} -e ${pkgs.lf}/bin/lf";
-          "${modifier}+Ctrl+g" =
+          "${modifier}+Shift+f" = "exec ${terminal} -e ${pkgs.lf}/bin/lf";
+          "${modifier}+Shift+g" =
             "exec ${terminal} -e ${pkgs.lazygit}/bin/lazygit";
-          "${modifier}+Ctrl+l" = "exec ${pkgs.logseq}/bin/logseq";
-          "${modifier}+Ctrl+n" = "exec ${pkgs.nitrokey-app}/bin/nitrokey-app";
+          "${modifier}+Shift+l" = "exec ${pkgs.logseq}/bin/logseq";
+          "${modifier}+Shift+n" = "exec ${pkgs.nitrokey-app}/bin/nitrokey-app";
           "${modifier}+Shift+s" = "exec ${pkgs.flameshot}/bin/flameshot gui";
-          "${modifier}+Ctrl+t" =
+          "${modifier}+Shift+t" =
             "exec ${terminal} -e ${pkgs.bpytop}/bin/bpytop";
-          "${modifier}+Ctrl+w" = "exec ${pkgs.wireshark}/bin/wireshark";
+          "${modifier}+Shift+w" = "exec ${pkgs.wireshark}/bin/wireshark";
 
           "XF86AudioMute" =
             "exec ${pkgs.alsa-utils}/bin/amixer set Master toggle";
           "XF86AudioLowerVolume" =
-            "exec ${pkgs.alsa-utils}/bin/amixer set Master 4%-";
+            "exec ${pkgs.alsa-utils}/bin/amixer set Master 1%-";
           "XF86AudioRaiseVolume" =
-            "exec ${pkgs.alsa-utils}/bin/amixer set Master 4%+";
+            "exec ${pkgs.alsa-utils}/bin/amixer set Master 1%+";
 
           #"XF86AudioPause" = "exec ${pkgs.mpc-cli}/bin/mpc toggle";
           #"XF86AudioPrev" = "exec ${pkgs.mpc-cli}/bin/mpc prev";
           #"XF86AudioNext" = "exec ${pkgs.mpc-cli}/bin/mpc next";
 
           "XF86MonBrightnessDown" =
-            "exec ${pkgs.brightnessctl}/bin/brightnessctl set 4%-";
+            "exec ${pkgs.brightnessctl}/bin/brightnessctl set 2%-";
           "XF86MonBrightnessUp" =
-            "exec ${pkgs.brightnessctl}/bin/brightnessctl set 4%+";
+            "exec ${pkgs.brightnessctl}/bin/brightnessctl set 2%+";
         };
       };
     };
