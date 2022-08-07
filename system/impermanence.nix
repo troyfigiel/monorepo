e21c04e445ecab6aa606fab818a81dad63186c74
Troy Figiel <troy.figiel@gmail.com>
2022-08-07 16:50:39 +0200

    Comment out impermanence module for now

--- system/impermanence.nix
+++ system/impermanence.nix
@@ -1,8 +1,11 @@
 {
   # TODO: Add a bunch of directories and files.
   # TODO: Create a tmpfs partition.
-  environment.persistence."/nix/state" = {
-    hideMounts = true;
-    users.troy = { files = [ "impermanence-test-file" ]; };
-  };
+  # This does not work nicely on a normal partition, because the bind
+  # mount leaves behind files that on the next boot prevent impermanence
+  # from setting up bind mounts again.
+  # environment.persistence."/nix/state" = {
+  #   hideMounts = true;
+  #   users.troy = { files = [ "impermanence-test-file" ]; };
+  # };
 }
