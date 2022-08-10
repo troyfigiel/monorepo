c89d7e5727942b9802ab72a937d0bd6422d7738a
Troy Figiel <troy.figiel@gmail.com>
2022-08-10 22:53:09 +0200

    Add various directories to persistence

--- hosts/inspiron/modules/impermanence.nix
+++ hosts/inspiron/modules/impermanence.nix
@@ -1,7 +1,12 @@
 {
   environment.persistence."/nix/persist" = {
     hideMounts = true;
-    directories = [ "/etc/nixos" "/etc/ssh" "/var/log" ];
+    directories = [
+      "/etc/NetworkManager/system-connections"
+      "/etc/nixos"
+      "/etc/ssh"
+      "/var/log"
+    ];
 
     files = [ "/etc/machine-id" ];
 
@@ -11,7 +16,22 @@
     };
 
     users.troy = {
-      directories = [ "Documents" "Downloads" "Media" "Projects" ];
+      directories = [
+        # Signal stores its data in the .config directory.
+        # See: https://github.com/signalapp/Signal-Desktop/issues/4975
+        ".config/Signal"
+        ".config/whatsapp-for-linux"
+        ".cache/mozilla"
+        ".mozilla"
+        ".wallpapers"
+        ".local/share/atuin"
+        ".ssh/known_hosts"
+        ".vscode"
+        "Documents"
+        "Downloads"
+        "Media"
+        "Projects"
+      ];
     };
   };
 }
