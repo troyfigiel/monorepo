3f07ed4cf1ab0e8e3244fa853fb65bb43e3d5b56
Troy Figiel <troy.figiel@gmail.com>
2022-09-14 10:41:18 +0200

    Remove old systemd services

--- hosts/ins/users/troy/modules/git/default.nix
+++ hosts/ins/users/troy/modules/git/default.nix
@@ -27,28 +27,4 @@
     enable = true;
     settings = { disableStartupPopups = true; };
   };
-
-  systemd.user.services = {
-    clone-emacs-config = {
-      Unit = {
-        Description = "Service that clones my Emacs config to .emacs.d";
-      };
-
-      Service = {
-        Type = "oneshot";
-        ExecStart =
-          "${pkgs.git}/bin/git clone git@gitlab.com:troy.figiel/emacs.git /home/troy/.emacs.d";
-      };
-    };
-
-    clone-notes = {
-      Unit = { Description = "Service that clones my Zettelkasten notes"; };
-
-      Service = {
-        Type = "oneshot";
-        ExecStart =
-          "${pkgs.git}/bin/git clone git@gitlab.com:troy.figiel/zettelkasten.git /home/troy/zettelkasten";
-      };
-    };
-  };
 }
