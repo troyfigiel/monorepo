ef4408d9976f42867f09cf17164b9638db5ef435
Troy Figiel <troy.figiel@gmail.com>
2022-11-13 21:35:25 +0100

    Add pdf-tools to packages

--- modules/home-manager/my/editors/emacs/packages.nix
+++ modules/home-manager/my/editors/emacs/packages.nix
@@ -14,6 +14,14 @@
   ];
 
   programs.emacs.init.usePackage = {
+    pdf-tools = {
+      enable = true;
+      # TODO: This seems to be necessary to open a pdf file with pdf-view-mode automatically. I am not sure if this is specific to the Nix package or also is the case on other operating systems.
+      config = ''
+        (add-to-list 'auto-mode-alist '("\\.pdf\\'" . pdf-view-mode))
+      '';
+    };
+
     # TODO: This should not be here, but for some reason I cannot use (require 'no-littering) in either prelude or earlyInit of the usePackage definition.
     # I am not sure why this does not work. Maybe a bug in the home-manager module? This is a work-around for now.
     # I think the problem is that no-littering is trying to create a directory. This error is specific to no-littering.
