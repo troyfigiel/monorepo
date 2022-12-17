391227ed6dd44cb68c8a93f8f3e432eaa06f8b01
Troy Figiel <troy.figiel@gmail.com>
2022-12-17 22:06:42 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -32,9 +32,8 @@
     };
   };
 
-  outputs = { self, ... }@inputs:
-    let inherit (inputs.flake-parts.lib) mkFlake;
-    in mkFlake { inherit self; } {
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" ];
 
       imports = [
