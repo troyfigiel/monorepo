8b9165ddfc8c8041b50eb4ab771425df69a28398
Troy Figiel <troy.figiel@gmail.com>
2022-12-27 19:17:00 +0100

    Move reference projects to templates

--- templates/flake-module.nix
+++ templates/flake-module.nix
@@ -1,6 +1,13 @@
 {
-  flake.templates.jupyter = {
-    description = "Jupyter environment for data science";
-    path = ./jupyter;
+  flake.templates = {
+    jupyter = {
+      description = "Jupyter environment for data science";
+      path = ./jupyter;
+    };
+
+    test-suites = {
+      description = "Example Python project using a variety of test-suites";
+      path = ./test-suites;
+    };
   };
 }
