3288aa1f7fc9dbe71108cadffa65156c94e5f931
Troy Figiel <troy.figiel@gmail.com>
2022-12-28 11:04:52 +0100

    Remove Searx from cloud-server

--- hosts/cloud-server/default.nix
+++ hosts/cloud-server/default.nix
@@ -41,49 +41,17 @@
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
 
-    virtualHosts = {
-      "troyfigiel.com" = {
-        forceSSL = true;
-        enableACME = true;
-        serverAliases = [ "www.troyfigiel.com" ];
-        locations."/" = { root = pkgs.website; };
-      };
-
-      "search.troyfigiel.com" = {
-        forceSSL = true;
-        enableACME = true;
-        locations."/" = { proxyPass = "http://localhost:8080"; };
-        basicAuthFile = config.sops.secrets.searx-basic-auth-file.path;
-      };
+    virtualHosts."troyfigiel.com" = {
+      forceSSL = true;
+      enableACME = true;
+      serverAliases = [ "www.troyfigiel.com" ];
+      locations."/" = { root = pkgs.website; };
     };
   };
 
   security.acme = {
     acceptTerms = true;
     defaults.email = "troy.figiel@gmail.com";
-  };
-
-  # TODO: Searx works, but I tend to get bad search results. I need to figure out how to improve that.
-  # TODO: I am not sure how I can add Searx as my default search engine. It does not always seem to work properly. Is this because of the basic auth?
-  sops.secrets = {
-    searx-basic-auth-file = {
-      # nginx needs to be able to read the basic-auth-file to perform the authentication.
-      # TODO: Can I make this dependent on the nginx service?
-      owner = "nginx";
-    };
-    searx-environment-file = { };
-  };
-
-  services.searx = {
-    enable = true;
-    settings = {
-      server = {
-        port = 8080;
-        bind_adress = "0.0.0.0";
-        secret_key = "@SEARX_SECRET_KEY@";
-      };
-    };
-    environmentFile = config.sops.secrets.searx-environment-file.path;
   };
 
   # TODO: I have not checked yet whether everything works.
