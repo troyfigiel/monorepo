098c6ce24cf2c1f2cd6060fd7ef30cc137871147
Troy Figiel <troy.figiel@gmail.com>
2023-01-08 12:22:58 +0100

    Clean up flake

--- flake.nix
+++ flake.nix
@@ -9,30 +9,22 @@
       url = "github:serokell/deploy-rs";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     emacs-overlay = {
       url = "github:nix-community/emacs-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     flake-parts.url = "github:hercules-ci/flake-parts";
-
     hardware.url = "github:nixos/nixos-hardware";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     impermanence.url = "github:nix-community/impermanence";
-
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
     simple-nixos-mailserver = {
       url = "gitlab:simple-nixos-mailserver/nixos-mailserver/nixos-22.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     sops-nix = {
       url = "github:mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -42,18 +34,16 @@
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" ];
-
       imports = [ ./machines/flake-module.nix ./templates/flake-module.nix ];
-
       perSystem = { system, pkgs, ... }: {
         legacyPackages = import inputs.nixpkgs {
           inherit system;
-          config.allowUnfreePredicate = pkg:
-            builtins.elem (inputs.nixpkgs.lib.getName pkg) [ "skypeforlinux" ];
-
-          # TODO: Does it make sense setting this for x86_64 systems as well? Software that is not
-          # built for aarch64 does seem to work fine.
-          config.allowUnsupportedSystem = true;
+          config = {
+            allowUnfreePredicate = pkg:
+              builtins.elem (inputs.nixpkgs.lib.getName pkg)
+              [ "skypeforlinux" ];
+            allowUnsupportedSystem = true;
+          };
           overlays = [
             inputs.deploy-rs.overlay
             inputs.emacs-overlay.overlay
@@ -73,38 +63,34 @@
           ];
         };
 
-        apps = {
-          default = {
-            type = "app";
-            program = pkgs.writeShellApplication {
-              name = "monorepo";
-              runtimeInputs = with pkgs; [ nixos-rebuild deploy-rs ];
-              text = ''
-                if [[ "$EUID" != 0 ]]; then
-                   echo 'Root privileges are needed to activate the local NixOS configuration.'
-                   exit 1
-                fi
-                nix fmt
-                nix flake check --keep-going || exit 1
-                nix run .#infrastructure
-                nixos-rebuild switch --flake .
-                deploy . -s
-              '';
-            };
-          };
+        apps.default = {
+          type = "app";
+          program = pkgs.writeShellApplication {
+            name = "monorepo";
+            runtimeInputs = with pkgs; [
+              coreutils
+              deploy-rs
+              execline.bin
+              nixos-rebuild
+              terraform
+            ];
+            text = ''
+              if [[ "$EUID" != 0 ]]; then
+                 echo 'Root privileges are needed to activate the local NixOS configuration.'
+                 exit 1
+              fi
+              nix fmt
+              nix flake check --keep-going || exit 1
 
-          infrastructure = {
-            type = "app";
-            program = pkgs.writeShellApplication {
-              name = "monorepo-terraform";
-              runtimeInputs = with pkgs; [ coreutils execline.bin terraform ];
-              text = ''
-                cd terraform || exit 1
-                terraform init
-                terraform apply
-                terraform output -json > outputs.json
-              '';
-            };
+              cd terraform || exit 1
+              terraform init
+              terraform apply
+              terraform output -json > outputs.json
+              cd ..
+
+              nixos-rebuild switch --flake .
+              deploy . -s
+            '';
           };
         };
 
@@ -117,13 +103,11 @@
             python3
             python3Packages.flake8
             python3Packages.python-lsp-server
+            terraform
             texlive.combined.scheme-full
           ];
         };
 
-        # TODO: I need to run touch $out for the linters, because builds need to have an output.
-        # This feels hacky. Is it the only way? It seems I am not the only one to do so:
-        # https://github.com/kira-bruneau/flake-linter/blob/main/lib/make-flake-linter/default.nix
         checks = {
           deadnix = pkgs.runCommand "checks-deadnix" {
             buildInputs = [ pkgs.deadnix ];
